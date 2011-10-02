469f55832a3cdbe219bba2a50d83e6a0fe66a07a
root <root@niftyserver.(none)>
2011-10-03 00:04:26 +0100

    Added submitjob script to correct place

--- Makefile
+++ Makefile
@@ -8,6 +8,8 @@ clean:
 	
 install:
 	cp backend.py /usr/libexec/cups/backend/cloudprint
+	mkdir -p /usr/lib/cloudprint/
+	cp submitjob.py /usr/lib/cloudprint/
 	cp cloudprint.ppd /usr/share/cups/model/CloudPrint.ppd
 	chown root:root /usr/libexec/cups/backend/cloudprint
 	chmod 700 /usr/libexec/cups/backend/cloudprint
--- backend.py
+++ backend.py
@@ -51,7 +51,7 @@ logfile.write("Printing file " + printFile + "\n")
 pdfFile = printFile+".pdf"
 
 subprocess.call(["ps2pdf", printFile, pdfFile])
-submitjobpath = sys.path[0] + "/submitjob.py"
+submitjobpath = "/usr/lib/cloudprint/" + "submitjob.py"
 logfile.write("Running " +  submitjobpath  + "\n")
 logfile.write("Converted to PDF as "+ pdfFile + "\n")
 logfile.write("Sending "+ pdfFile + " to cloud\n")
