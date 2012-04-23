9d055e2faaaa5d9123a6454cf9bdfa65d0fce959
Simon Cadman <src@niftiestsoftware.com>
2012-04-23 21:51:36 +0100

    Specify python 2.7

--- setupcloudprint.py
+++ setupcloudprint.py
@@ -1,4 +1,4 @@
-#! /usr/bin/env python2
+#! /usr/bin/env python2.7
 
 import sys, getpass, cups
 from config import Config
--- submitjob.py
+++ submitjob.py
@@ -1,4 +1,4 @@
-#! /usr/bin/env python2
+#! /usr/bin/env python2.7
 #    CUPS Cloudprint - Print via Google Cloud Print                          
 #    Copyright (C) 2011 Simon Cadman
 #
