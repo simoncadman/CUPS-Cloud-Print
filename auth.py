759181a4c8fef8a4f35b9233bd0c297c40221df9
Simon Cadman <src@niftiestsoftware.com>
2012-06-18 21:06:35 +0100

    Added time module

--- auth.py
+++ auth.py
@@ -14,7 +14,7 @@
 #    You should have received a copy of the GNU General Public License    
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-import urllib, urllib2, mimetools
+import urllib, urllib2, mimetools, time
 
 class Auth():
   SERVICE = 'cloudprint'
