a71d0e99371edeed1384438a75570778959a99a5
Simon Cadman <src@niftiestsoftware.com>
2012-08-11 21:02:25 +0100

    Added PPD refresh postinstall script

---
+++ refreshppd.py
@@ -0,0 +1,15 @@
+#! /usr/bin/env python2.7
+
+import sys, os, grp, getpass, cups, subprocess
+
+connection = cups.Connection()
+cupsprinters = connection.getPrinters()
+
+for device in cupsprinters:
+  if ( cupsprinters[device]["device-uri"].find("cloudprint://") == 0 ):
+    print "Updating " + cupsprinters[device]["printer-info"]
+    
+    p = subprocess.Popen(["lpadmin", "-p", cupsprinters[device]["printer-info"], "-m", "CloudPrint.ppd"], stdout=subprocess.PIPE)
+    output = p.communicate()[0]
+    result = p.returncode
+    sys.stderr.write(output)
