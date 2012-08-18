c6f6ef65110a89ffd484ca9fa2d86e5a7350b48b
Simon Cadman <src@niftiestsoftware.com>
2012-08-18 01:35:03 +0100

    Added mimetools back

--- printer.py
+++ printer.py
@@ -13,7 +13,7 @@
 #
 #    You should have received a copy of the GNU General Public License    
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-import json, urllib, os, mimetypes, base64
+import json, urllib, os, mimetypes, base64, mimetools
 from auth import Auth
 from urlparse import urlparse
 
