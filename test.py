495b3b52fa208618509a9a95a70d27544587247b
Simon Cadman <src@niftiestsoftware.com>
2012-12-29 23:54:05 +0000

    Simple simplejson, added cups include to test script

--- test.py
+++ test.py
@@ -15,6 +15,6 @@
 #    You should have received a copy of the GNU General Public License    
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-import sys
+import sys, cups
 from auth import Auth
 from printer import Printer
