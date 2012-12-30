b26a7197ec52e882bb8dcbd11fffe82df100f24e
Simon Cadman <src@niftiestsoftware.com>
2012-12-30 00:21:58 +0000

    Include cups library in test

--- test.py
+++ test.py
@@ -15,6 +15,8 @@
 #    You should have received a copy of the GNU General Public License    
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-import sys
+import sys, cups
 from auth import Auth
 from printer import Printer
+
+# setup basic printers
