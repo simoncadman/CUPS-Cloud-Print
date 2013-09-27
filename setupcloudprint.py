5ab76dba66bf0414d6b2665f75de179f4021483c
Simon Cadman <src@niftiestsoftware.com>
2013-09-27 23:17:16 +0100

    Added test capabilies file to ignore list

--- setupcloudprint.py
+++ setupcloudprint.py
@@ -21,7 +21,7 @@ from printer import Printer
 
 if len(sys.argv) == 2 and sys.argv[1] == 'version':
     # line below is replaced on commit
-    CCPVersion = "20130927 221035"
+    CCPVersion = "20130927 221716"
     print "CUPS Cloud Print Setup Script Version " + CCPVersion
     sys.exit(0)
 
