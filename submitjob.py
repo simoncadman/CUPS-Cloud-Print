bae9c3fe624701bf8e18fa01c65e775037a41bc2
Simon Cadman <src@niftiestsoftware.com>
2013-09-27 23:07:41 +0100

    Added more test capabilities

--- submitjob.py
+++ submitjob.py
@@ -21,7 +21,7 @@ from printer import Printer
 
 if len(sys.argv) == 2 and sys.argv[1] == 'version':
     # line below is replaced on commit
-    CCPVersion = "20130927 220020"
+    CCPVersion = "20130927 221035"
     print "CUPS Cloud Print Submit Job Version " + CCPVersion
     sys.exit(0)
     
