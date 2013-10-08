b067f95147a8b2287ca4790a39daa4ed532f9336
Simon Cadman <src@niftiestsoftware.com>
2013-10-08 23:31:53 +0100

    Depend on pdf90 and pdf270 binaries for rpm

--- backend.py
+++ backend.py
@@ -20,7 +20,7 @@ progname = 'cloudprint'
 
 if len(sys.argv) == 2 and sys.argv[1] == 'version':
     # line below is replaced on commit
-    CCPVersion = "20131008 210525"
+    CCPVersion = "20131008 223154"
     print "CUPS Cloud Print CUPS Backend Version " + CCPVersion
     sys.exit(0)
 
