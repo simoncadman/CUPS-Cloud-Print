a1615348f2b352d69783c41d86e852d32957d33d
Simon Cadman <src@niftiestsoftware.com>
2013-12-11 11:43:40 +0000

    Only fix permissions of cloudprint.conf if exists

--- backend.py
+++ backend.py
@@ -20,7 +20,7 @@ progname = 'cloudprint'
 
 if len(sys.argv) == 2 and sys.argv[1] == 'version':
     # line below is replaced on commit
-    CCPVersion = "20131211 114027"
+    CCPVersion = "20131211 114340"
     print "CUPS Cloud Print CUPS Backend Version " + CCPVersion
     sys.exit(0)
 
