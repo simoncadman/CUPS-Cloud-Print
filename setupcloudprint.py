d41d657590c2a885282100942dd9362949817d24
Simon Cadman <src@niftiestsoftware.com>
2013-12-22 23:39:35 +0000

    Changed github repo variable to make more consistent

--- setupcloudprint.py
+++ setupcloudprint.py
@@ -21,7 +21,7 @@ from printer import Printer
 
 if len(sys.argv) == 2 and sys.argv[1] == 'version':
     # line below is replaced on commit
-    CCPVersion = "20131222 232806"
+    CCPVersion = "20131222 233935"
     print "CUPS Cloud Print Setup Script Version " + CCPVersion
     sys.exit(0)
 
