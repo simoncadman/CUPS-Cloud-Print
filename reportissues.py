85f1717e9b0f55e7f13afa6bb5707821b7cc2974
Simon Cadman <src@niftiestsoftware.com>
2013-12-22 23:28:06 +0000

    Fixed quote and pass more data to php script

--- reportissues.py
+++ reportissues.py
@@ -19,7 +19,7 @@ import sys, os, subprocess
 
 if len(sys.argv) == 2 and sys.argv[1] == 'version':
     # line below is replaced on commit
-    CCPVersion = "20131222 232015"
+    CCPVersion = "20131222 232806"
     print "CUPS Cloud Print Issue Reporting Script Version " + CCPVersion
     sys.exit(0)
 
