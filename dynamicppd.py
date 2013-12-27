68f7b8cd3853450c2fa68f4bef6224faba7010dd
Simon Cadman <src@niftiestsoftware.com>
2013-12-27 00:05:28 +0000

    Depend on python 2.6 or 2.7

--- dynamicppd.py
+++ dynamicppd.py
@@ -19,7 +19,7 @@ import sys, os, locale
 
 if len(sys.argv) == 2 and sys.argv[1] == 'version':
     # line below is replaced on commit
-    CCPVersion = "20131226 235751"
+    CCPVersion = "20131227 000528"
     print "CUPS Cloud Print Dynamic PPD Generator Version " + CCPVersion
     sys.exit(0)
 
