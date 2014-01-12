404ab74edfa2decb1e3ac20bf9fd66156d50c3aa
Simon Cadman <src@niftiestsoftware.com>
2014-01-12 21:08:22 +0000

    Fixed makefile to copy backend and dynamic ppd files instead of symlinks

--- backend.py
+++ backend.py
@@ -20,7 +20,7 @@ progname = 'cloudprint'
 
 if len(sys.argv) == 2 and sys.argv[1] == 'version':
     # line below is replaced on commit
-    CCPVersion = "20140112 163426"
+    CCPVersion = "20140112 210822"
     print "CUPS Cloud Print CUPS Backend Version " + CCPVersion
     sys.exit(0)
 
