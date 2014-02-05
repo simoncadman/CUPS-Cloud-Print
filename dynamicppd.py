c2fe36ff1019652657ae96ee6835bc478d2d7250
Simon Cadman <src@niftiestsoftware.com>
2014-02-05 23:58:35 +0000

    Added prerm script to delete pyc files before removal

--- dynamicppd.py
+++ dynamicppd.py
@@ -19,7 +19,7 @@ import sys, os, locale, logging
 
 if len(sys.argv) == 2 and sys.argv[1] == 'version':
     # line below is replaced on commit
-    CCPVersion = "20140203 225614"
+    CCPVersion = "20140205 235835"
     print "CUPS Cloud Print Dynamic PPD Generator Version " + CCPVersion
     sys.exit(0)
 
