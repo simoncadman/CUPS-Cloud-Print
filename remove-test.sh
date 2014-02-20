51df32e0465ffd94c642032423bd9ee7c22b8061
Simon Cadman <src@niftiestsoftware.com>
2014-02-20 13:31:58 +0000

    Check if more files exist after removal

--- remove-test.sh
+++ remove-test.sh
@@ -1,9 +1,31 @@
 #! /bin/bash
 
 set -e
+returnval=0
 
 if [[ -d '/usr/share/cloudprint-cups/' ]]; then
     echo "/usr/share/cloudprint-cups/ dir already exists: "
     ls -alR /usr/share/cloudprint-cups/
-    exit 1
+    returnval=1
 fi
+
+cupsdir="/usr/lib/cups"
+
+if [[ -d '/usr/libexec/cups' ]]; then
+    cupsdir="/usr/libexec/cups"
+fi
+
+
+if [[ -e "$cupsdir/backend/cloudprint" ]]; then
+    echo "$cupsdir/backend/cloudprint file already exists: "
+    ls -alR $cupsdir/backend/cloudprint
+    returnval=1
+fi
+
+if [[ -e "$cupsdir/driver/cupscloudprint" ]]; then
+    echo "$cupsdir/driver/cupscloudprint file already exists: "
+    ls -alR $cupsdir/driver/cupscloudprint
+    returnval=1
+fi
+
+exit $returnval
