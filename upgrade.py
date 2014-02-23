a22066d3b6437c15192a1c9acfe392ae3d6cdd9d
Simon Cadman <src@niftiestsoftware.com>
2014-02-23 15:42:21 +0000

    Use group of ppd dir instead of config file

--- upgrade.py
+++ upgrade.py
@@ -34,7 +34,7 @@ if __name__ == '__main__': # pragma: no cover
     printerItem = Printer(requestors)
         
     # line below is replaced on commit
-    CCPVersion = "20140223 145058"
+    CCPVersion = "20140223 154234"
 
     if len(sys.argv) == 2 and sys.argv[1] == 'version':
         print "CUPS Cloud Print Upgrade Script Version " + CCPVersion
