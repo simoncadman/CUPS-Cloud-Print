cac2817f09b86e93b9fee26acd96a247f10af24d
Simon Cadman <src@niftiestsoftware.com>
2014-03-09 22:04:08 +0000

    Moved coveragerc back to root

--- reportissues.py
+++ reportissues.py
@@ -29,7 +29,7 @@ if __name__ == '__main__': # pragma: no cover
     Utils.SetupLogging()
     
     # line below is replaced on commit
-    CCPVersion = "20140309 220149"
+    CCPVersion = "20140309 220408"
     Utils.ShowVersion(CCPVersion)
     
     requestors, storage = Auth.SetupAuth(True)
