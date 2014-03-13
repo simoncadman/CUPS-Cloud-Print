173243ebbd74fe53160bea412f0e2e3d1f052dfd
Simon Cadman <src@niftiestsoftware.com>
2014-03-13 22:40:39 +0000

    Named getbackenddescriptionforprinter test

--- setupcloudprint.py
+++ setupcloudprint.py
@@ -23,7 +23,7 @@ if __name__ == '__main__': # pragma: no cover
     Utils.SetupLogging()
     
     # line below is replaced on commit
-    CCPVersion = "20140313 223828"
+    CCPVersion = "20140313 224039"
     Utils.ShowVersion(CCPVersion)
     
     unattended = False
