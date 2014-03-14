b449ab7ee575e05b4fb4be5bb64020f18f717245
Simon Cadman <src@niftiestsoftware.com>
2014-03-14 22:09:11 +0000

    Test rotating pdf in travis-ci

--- backend.py
+++ backend.py
@@ -32,7 +32,7 @@ if __name__ == '__main__': # pragma: no cover
     Utils.SetupLogging()
     
     # line below is replaced on commit
-    CCPVersion = "20140314 215719"
+    CCPVersion = "20140314 220911"
     Utils.ShowVersion(CCPVersion)
     
     requestors, storage = Auth.SetupAuth(False)
--- setupcloudprint.py
+++ setupcloudprint.py
@@ -23,7 +23,7 @@ if __name__ == '__main__': # pragma: no cover
     Utils.SetupLogging()
     
     # line below is replaced on commit
-    CCPVersion = "20140314 215719"
+    CCPVersion = "20140314 220911"
     Utils.ShowVersion(CCPVersion)
     
     unattended = False
