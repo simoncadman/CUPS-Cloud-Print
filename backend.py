8dd9b1d5a216dd9b13b13561e0cfcff138795500
Simon Cadman <src@niftiestsoftware.com>
2014-03-14 21:47:35 +0000

    Chmod testfiles dir to 777

--- backend.py
+++ backend.py
@@ -32,7 +32,7 @@ if __name__ == '__main__': # pragma: no cover
     Utils.SetupLogging()
     
     # line below is replaced on commit
-    CCPVersion = "20140314 213008"
+    CCPVersion = "20140314 214735"
     Utils.ShowVersion(CCPVersion)
     
     requestors, storage = Auth.SetupAuth(False)
