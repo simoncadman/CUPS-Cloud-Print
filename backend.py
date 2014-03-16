0bfaa3453aeb294f77f4b316094c307b7d8a09ed
Simon Cadman <src@niftiestsoftware.com>
2014-03-16 14:04:41 +0000

    Set the list files script to 755

--- backend.py
+++ backend.py
@@ -32,7 +32,7 @@ if __name__ == '__main__': # pragma: no cover
     Utils.SetupLogging()
     
     # line below is replaced on commit
-    CCPVersion = "20140316 135607"
+    CCPVersion = "20140316 140441"
     Utils.ShowVersion(CCPVersion)
     
     requestors, storage = Auth.SetupAuth(False)
