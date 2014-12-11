2caa73961c810d647d27c2996c20c2f01d335966
Simon Cadman <src@niftiestsoftware.com>
2014-12-11 17:20:51 +0000

    Restart dbus if cups is already running

--- testing/full-test.sh
+++ testing/full-test.sh
@@ -40,7 +40,7 @@ if [[ "`whoami`" == "root"  ]]; then
        hash systemctl && ( systemctl start cups || cupsd )
        
        # start via 'start' if exists
-       hash start && ( ( start cups && restart dbus ) || cupsd )
+       hash start && ( ( start cups ; restart dbus ) || cupsd )
        
 fi
 
