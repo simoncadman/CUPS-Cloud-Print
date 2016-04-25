8744ce12bc2fda66d93fea5ed27b287769741913
Simon Cadman <src@niftiestsoftware.com>
2016-04-25 01:32:42 +0100

    Syntax fix

--- ccputils.py
+++ ccputils.py
@@ -119,7 +119,7 @@ class Utils:
         # add dirs to path if exist
         extrapaths = ['/opt/Ghostscript/bin','/opt/ImageMagick/bin']
         for extrapath in extrapaths:
-            if os.path.exists(extrapath)
+            if os.path.exists(extrapath):
                 shellpath = shellpath + os.pathsep + extrapath
         return shellpath
 
